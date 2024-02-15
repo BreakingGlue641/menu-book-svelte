90300c34b0bc2074b956413944c800918ea6acad
BreakingGlue641 <143070880+BreakingGlue641@users.noreply.github.com>
2024-02-15 08:41:31 +0000

    MenuTile

---
+++ src/lib/component/MenuTile.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    export let image1: string;
+    export let name2: string;
+    export let description3: string;
+</script>
+
+<div class="menu-tile">
+    <div class="menu-photo">
+      <img
+        src={image1} alt={name2}>
+    </div>
+    <div class="menu-tile-name">{name2}</div>
+    <div class="menu-tile-description">
+      {description3}
+    </div>
+</div>
+
+<style>
+      div.menu-tile {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 16px;
+  }
+
+  div.menu-photo img {
+    aspect-ratio: 1;
+    object-fit: cover;
+    max-width: 100%;
+  }
+
+  div.menu-tile-name {
+    font-weight: 500;
+    font-size: 16px;
+    padding: 8px 16px;
+    align-self: stretch;
+    text-align: center;
+  }
+
+  div.menu-tile-description {
+    font-size: 12px;
+    align-self: stretch;
+    text-align: center;
+    padding: 0px 16px;
+    height: 60px;
+    padding-bottom: 16px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
-
+<script>
+  import MenuTile from '../lib/component/MenuTile.svelte';
+</script>
 
 <head>
   <meta charset="utf-8" />
@@ -8,6 +9,8 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=true>
   <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500&display=swap" rel="stylesheet">
 </head>
+
+
 
 <body>
   <div class="appbar">Kongkow Cafe</div>
@@ -20,83 +23,28 @@
   </div>
   <div class="menu-area">
     <div class="menu-row">
-      <div class="menu-tile">
-        <div class="menu-photo">
-          <img
-            src="https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg?resize=960%2C503"
-            alt="salad">
-        </div>
-        <div class="menu-tile-name">Avocado Pesto Salad</div>
-        <div class="menu-tile-description">
-          “Home-grown vegetables with loads of avocado, topped with mouthwatering pesto salad”
-        </div>
-        <div class="menu-price-row">
-          <span class="price-description"></span>
-          <span>35</span>
-        </div>
-        <div class="menu-price-row">
-          <span class="price-description">+ Grilled Salmon</span>
-          <span>55</span>
-        </div>
-      </div>
-      <div class="menu-tile">
-        <div class="menu-photo">
-          <img src="https://www.recipetineats.com/wp-content/uploads/2023/05/Garlic-cheese-pizza_9.jpg" alt="pizza">
-        </div>
-        <div class="menu-tile-name">Triple Cheese Pizza</div>
-        <div class="menu-tile-description">
-          “Indulge yourself in this simple yet delicious delicacy”
-        </div>
-        <div class="menu-price-row">
-          <span class="price-description">1 Slice</span>
-          <span>15</span>
-        </div>
-        <div class="menu-price-row">
-          <span class="price-description">Pan (6 slices)</span>
-          <span>60</span>
-        </div>
-      </div>
-    </div>
-    <div class="menu-row">
-      <div class="menu-tile">
-        <div class="menu-photo">
-          <img
-            src="https://images.theconversation.com/files/525691/original/file-20230511-19-w9pz4k.jpg?ixlib=rb-1.1.0&rect=5%2C2%2C1905%2C1276&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip"
-            alt="wine">
-        </div>
-        <div class="menu-tile-name">Seasonal Wine</div>
-        <div class="menu-tile-description">
-          “A great meal becomes perfect when combined with a good wine”
-        </div>
-        <div class="menu-price-row">
-          <span class="price-description">Glass</span>
-          <span>100</span>
-        </div>
-        <div class="menu-price-row">
-          <span class="price-description">Bottle</span>
-          <span>1500</span>
-        </div>
-      </div>
-      <div class="menu-tile">
-        <div class="menu-photo">
-          <img
-            src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_512,h_512/https://www.adityabirlacapital.com/healthinsurance/active-together/wp-content/uploads/2019/12/How-Are-Espressos-Good-For-Health_blog-1.gif"
-            alt="coffee">
-        </div>
-        <div class="menu-tile-name">Coffee</div>
-        <div class="menu-tile-description">
-          “The best coffee you can have from the land of Indonesia”
-        </div>
-        <div class="menu-price-row">
-          <span class="price-description">Americano</span>
-          <span>25</span>
-        </div>
-        <div class="menu-price-row">
-          <span class="price-description">Latte</span>
-          <span>30</span>
-        </div>
-      </div>
-    </div>
+      <MenuTile 
+      image1={"https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg?resize=960%2C503"} 
+      name2={"Avocado Pesto Salad"} 
+      description3={"“Home-grown vegetables with loads of avocado, topped with mouthwatering pesto salad”"}
+      />
+      <MenuTile 
+      image1={"https://www.recipetineats.com/wp-content/uploads/2023/05/Garlic-cheese-pizza_9.jpg"}
+      name2={"Triple Cheese Pizza"} 
+      description3={"“Indulge yourself in this simple yet delicious delicacy”"}
+      />
+      <MenuTile 
+      image1={"https://images.theconversation.com/files/525691/original/file-20230511-19-w9pz4k.jpg?ixlib=rb-1.1.0&rect=5%2C2%2C1905%2C1276&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip"} 
+      name2={"Seasonal Wine"} 
+      description3={"“A great meal becomes perfect when combined with a good wine”"}
+      />
+      <MenuTile 
+      image1={"https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_512,h_512/https://www.adityabirlacapital.com/healthinsurance/active-together/wp-content/uploads/2019/12/How-Are-Espressos-Good-For-Health_blog-1.gif"} 
+      name2={"Coffee"} 
+      description3={"“The best coffee you can have from the land of Indonesia”"}
+      />
+
+</div>
   </div>
 </body>
 
@@ -165,35 +113,7 @@
     gap: 16px;
   }
 
-  div.menu-tile {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding-bottom: 16px;
-  }
 
-  div.menu-photo img {
-    aspect-ratio: 1;
-    object-fit: cover;
-    max-width: 100%;
-  }
-
-  div.menu-tile-name {
-    font-weight: 500;
-    font-size: 16px;
-    padding: 8px 16px;
-    align-self: stretch;
-    text-align: center;
-  }
-
-  div.menu-tile-description {
-    font-size: 12px;
-    align-self: stretch;
-    text-align: center;
-    padding: 0px 16px;
-    height: 60px;
-    padding-bottom: 16px;
-  }
 
   div.menu-price-row {
     display: flex;
