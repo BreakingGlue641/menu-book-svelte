c990368012111510acafd0146ef8bda1dd06e541
BreakingGlue641 <143070880+BreakingGlue641@users.noreply.github.com>
2024-04-25 07:58:47 +0000

    order.ts

--- src/lib/store/order.ts
+++ src/lib/store/order.ts
@@ -1,14 +1,25 @@
 import { derived, writable } from "svelte/store";
 
-export const saladCounter = writable(0);
-export const pizzaCounter = writable(0);
-export const wineCounter = writable(0);
-export const coffeeCounter = writable(0);
+export const order = writable<{ [key: string]: number }>({});
 
 // #1
 export const totalOrderCount = derived(
-    [saladCounter, pizzaCounter, wineCounter, coffeeCounter],
-    (c) => {
-        return c[0] + c[1] + c[2] + c[3];
+    order,
+    (o) => {
+        return Object.values(o).reduce((prev, curr) => prev + curr , 0);
     }
-);
+)
+
+
+// export const saladCounter = writable(0);
+// export const pizzaCounter = writable(0);
+// export const wineCounter = writable(0);
+// export const coffeeCounter = writable(0);
+
+// // #1
+// export const totalOrderCount = derived(
+//     [saladCounter, pizzaCounter, wineCounter, coffeeCounter],
+//     (c) => {
+//         return c[0] + c[1] + c[2] + c[3];
+//     }
+// );
